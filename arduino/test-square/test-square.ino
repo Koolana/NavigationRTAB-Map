979d05e9da2c3cef94285d45aa531bf9739690c5
Nikolay Andreychik <kolya.andreychik@gmail.com>
2021-07-30 12:52:22 -0700

    clockwise movement

--- arduino/test-square/test-square.ino
+++ arduino/test-square/test-square.ino
@@ -39,20 +39,20 @@ byte encoderRPinALast;
 byte encoderLPinALast;
 double wheelSpeedR = 0;  // Скорость правого колеса с энкодера
 double wheelSpeedL = 0;  // Скорость левого колеса с энкодера
-unsigned long wheelImpR = 0; // число импульсов с энкодера правого колеса 
-unsigned long wheelImpL = 0; // число импульсов с энкодера левого колеса 
+unsigned long wheelImpR = 0; // число импульсов с энкодера правого колеса
+unsigned long wheelImpL = 0; // число импульсов с энкодера левого колеса
 
 //PID variables
 double Motor_2[3]={0.1,3,0};                //PID parameters [P,I,D]
 double Setpoint1,Input1,Output1;                   //PID input&output values for Motor1
 double Setpoint2,Input2,Output2;                   //PID input&output values for Motor2
 
-PID myPID1(&Input1,&Output1,&Setpoint1,Motor_2[0],Motor_2[1],Motor_2[2],DIRECT);  
+PID myPID1(&Input1,&Output1,&Setpoint1,Motor_2[0],Motor_2[1],Motor_2[2],DIRECT);
 PID myPID2(&Input2,&Output2,&Setpoint2,Motor_2[0],Motor_2[1],Motor_2[2],DIRECT);
 
 // Timer variables
 const long Timer1Interval=100000;                                // 100 ms = 10 times per sec - Timer interrupt interval
-double dT = double(Timer1Interval)/1000000;           // период счета 
+double dT = double(Timer1Interval)/1000000;           // период счета
 
 //Motor control variables
 const int MotorRdir = 4;  // 52    //Right motor Direction Control pin
@@ -102,8 +102,8 @@ void printValue(double val, const char* namVal = NULL, bool withSignAndDouble = 
 void setup() {
    Init();
 }
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Главный цикл ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
-void loop() { 
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Главный цикл ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+void loop() {
   // --------------- Чтение порта --------------------
   get_messages_from_Serial();
   // --------------- Смена уставки скорости ----------
@@ -113,9 +113,9 @@ void loop() {
  //loop ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 void Motor() {
   CheckSettingsSpeed();
-  PIDMovement(SetSpeedR,SetSpeedL);  // передается линейная скорость колес, в м/сек  
+  PIDMovement(SetSpeedR,SetSpeedL);  // передается линейная скорость колес, в м/сек
 }
-void SetSpeed(double LinearVelocity, double AngularVelocity) { 
+void SetSpeed(double LinearVelocity, double AngularVelocity) {
   SetSpeedR = (((2*LinearVelocity)+(AngularVelocity*L))/(2*R))*R;   //M/S - линейная скорость колеса
   SetSpeedL = (((2*LinearVelocity)-(AngularVelocity*L))/(2*R))*R;
 }
@@ -141,7 +141,7 @@ void Init() {
   while (!Serial) ; // while the Serial stream is not open, do nothing
   MotorsInit();
   EncoderInit();//Initialize encoder
-  PIDInit();  
+  PIDInit();
 }
 
 void PIDInit() {
@@ -150,42 +150,42 @@ void PIDInit() {
   myPID1.SetOutputLimits(0,255);
   myPID2.SetOutputLimits(0,255);
 }
-void MotorsInit() { //Initialize motors variables 
+void MotorsInit() { //Initialize motors variables
   DirectionR = LOW;
   DirectionL = LOW;
   SetSpeedR = 0;
   SetSpeedL = 0;
-  
+
   pinMode(MotorRdir, OUTPUT);
   pinMode(MotorLdir, OUTPUT);
   pinMode(MotorRpwm, OUTPUT);
   pinMode(MotorLpwm, OUTPUT);
-  
+
   digitalWrite (MotorRdir, DirectionR);
   digitalWrite (MotorLdir, DirectionL);
   analogWrite (MotorRpwm, SetSpeedR);
   analogWrite (MotorLpwm, SetSpeedL);
 }
-void EncoderInit() { //Initialize encoder interruption 
+void EncoderInit() { //Initialize encoder interruption
 
   pinMode(encoderRpinA,INPUT);  // Right weel
-  pinMode(encoderRpinB,INPUT);  
+  pinMode(encoderRpinB,INPUT);
   pinMode(encoderLpinA,INPUT);  // Left weel
-  pinMode(encoderLpinB,INPUT);  
- 
+  pinMode(encoderLpinB,INPUT);
+
   // Привязка прерывания по импульсу энкодера
   attachInterrupt(digitalPinToInterrupt(encoderRpinA), WheelPulseR, RISING ); // вызов процедуры по прерыванию. Параметры: номер прерывания (не ножки), имя процедуры, состояние сигнала
   attachInterrupt(digitalPinToInterrupt(encoderLpinA), WheelPulseL, RISING );  // ЗАМЕНА, была ссылка на DecodeSpeedL
-  
+
   // Настройка таймера
   Timer1.initialize(Timer1Interval);
   Timer1.attachInterrupt(Timer_finish);
- 
+
 }
-void WheelPulseR() {   // Счетчик спиц правого колеса 
+void WheelPulseR() {   // Счетчик спиц правого колеса
   wheelImpR ++;
 }
-void WheelPulseL() {   // Счетчик спиц левого колеса 
+void WheelPulseL() {   // Счетчик спиц левого колеса
   wheelImpL ++;
 }
 void Timer_finish()  {
@@ -194,9 +194,9 @@ void Timer_finish()  {
   wheelSpeedL = double(wheelImpL / dT); // число импульсов за сек
 
   // пройденный колесом путь, м
-  wheelRightS = ((wheelSpeedR / 1450) * 2 * 3.14 * R);  // 663  // метры L = 2*PI*R*n/N 
-  wheelLeftS  = ((wheelSpeedL / 1450) * 2 * 3.14 * R);  //* 
-  
+  wheelRightS = ((wheelSpeedR / 1450) * 2 * 3.14 * R);  // 663  // метры L = 2*PI*R*n/N
+  wheelLeftS  = ((wheelSpeedL / 1450) * 2 * 3.14 * R);  //*
+
   // линейная скорость колеса
   wheelRightV = wheelRightS/ 1; // mетры за сек
   wheelLeftV  = wheelLeftS / 1;
@@ -204,7 +204,7 @@ void Timer_finish()  {
   // угловая скорость колеса
   omegaRight = wheelRightV/R;   // rad за сек
   omegaLeft  = wheelLeftV/R;
-  
+
   // фактическая линейная скорость центра робота
   V     = (R/2)*(omegaRight * (DirectionR ? -1 : 1) + omegaLeft * (DirectionL ? -1 : 1));//m/s
   // фактическая угловая скорость поворота робота
@@ -217,67 +217,67 @@ void Timer_finish()  {
   // проверка
   Vr = (((2*V)+(omega*L))/(2*R))*R; //M/S
   Vl = (((2*V)-(omega*L))/(2*R))*R;
-  
+
   wheelImpR = 0;
   wheelImpL = 0;
 
   if (state == 0 && x > 1) {
     state = 1;
     LinearVelocity = 0;
-    AngularVelocity = 0.2;
-    
+    AngularVelocity = -0.2;
+
     settingSpeed = true;
   }
 
-  if (state == 1 && yaw > 3.14 / 2) {;
+  if (state == 1 && yaw < -3.14 / 2) {;
     LinearVelocity = 0.2;
     AngularVelocity = 0;
-    
+
     settingSpeed = true;
   }
 
-  if (state == 1 && y > 1) {
+  if (state == 1 && y < -1) {
     state = 2;
     LinearVelocity = 0;
-    AngularVelocity = 0.2;
-    
+    AngularVelocity = -0.2;
+
     settingSpeed = true;
   }
 
-  if (state == 2 && yaw > 3.14) {;
+  if (state == 2 && yaw < -3.14) {;
     LinearVelocity = 0.2;
     AngularVelocity = 0;
-    
+
     settingSpeed = true;
   }
 
   if (state == 2 && x < 0) {
     state = 3;
     LinearVelocity = 0;
-    AngularVelocity = 0.2;
-    
+    AngularVelocity = -0.2;
+
     settingSpeed = true;
   }
 
-  if (state == 3 && yaw > 3 * 3.14 / 2) {;
+  if (state == 3 && yaw < -3 * 3.14 / 2) {;
     LinearVelocity = 0.2;
     AngularVelocity = 0;
-    
+
     settingSpeed = true;
   }
 
-  if (state == 3 && y < 0) {
+  if (state == 3 && y > 0) {
     state = 4;
     LinearVelocity = 0;
     AngularVelocity = 0.2;
-    
+
     settingSpeed = true;
   }
 
   if (state == 4) {
     LinearVelocity = 0;
     AngularVelocity = 0;
-    
+
     settingSpeed = true;
   }
 }
@@ -285,9 +285,9 @@ void Movement(int a,int b) {//move
   if (a < 13) {a = 0;}
   if (b < 13) {b = 0;}
   analogWrite (MotorRpwm,a);      //motor1 move forward at speed a
-  digitalWrite(MotorRdir,DirectionR);  
+  digitalWrite(MotorRdir,DirectionR);
   analogWrite (MotorLpwm,b);      //motor2 move forward at speed b
-  digitalWrite(MotorLdir,DirectionL);  
+  digitalWrite(MotorLdir,DirectionL);
 }
 
 //PID modules
@@ -302,7 +302,7 @@ void PIDMovement(double a,double b) {
 
   Setpoint1= (a * 255 /maxSpeed); // уставка скорости
   Setpoint2= (b * 255 /maxSpeed);
- 
+
   Input1= wheelRightV * 255 / maxSpeed;           // обратная связь ПИД-регулятора, м/сек
   Input2= wheelLeftV * 255 / maxSpeed;
 
@@ -343,17 +343,17 @@ void get_messages_from_Serial()
 
 //          printValue(LinearVelocity, "LinearVelocity");
 //          printValue(AngularVelocity, "AngularVelocity");
-          
+
           settingSpeed = true;
 
           break;
         }
-        
+
         case 'd'://если d, то печатаем текущие значения полно
         {
           printValue(V, "V");
           printValue(omega, "Omega");
-          
+
           printValue(yaw, "Yaw");
           printValue(x, "x");
           printValue(y, "y");
@@ -367,7 +367,7 @@ void get_messages_from_Serial()
         {
           printValue(V);  // linear velocity
           printValue(omega);  // angular velocity
-          
+
           printValue(yaw);  // yaw angle
           printValue(x);  // x position
           printValue(y);  // y position
@@ -382,7 +382,7 @@ void get_messages_from_Serial()
           AngularVelocity = 0;
 
           settingSpeed = true;
-          
+
           Serial.print("Stop command");
           Serial.print("\n");
 
@@ -401,13 +401,13 @@ void get_messages_from_Serial()
 
 void printValue(double val, const char* namVal, bool withSignAndDouble) {
   Serial.print(namVal==NULL ? "" : String(namVal) + ": ");
-  
+
   if (withSignAndDouble) {
     Serial.print(val>=0 ? "+" : "");
     Serial.print(val);
   } else {
     Serial.print(int(val));
   }
-   
+
   Serial.print("; ");
 }
